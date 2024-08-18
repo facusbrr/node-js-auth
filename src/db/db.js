a4b23ea3b5e07cb12f6a659ce63d26d88aa8a7a3
Gayoso Facundo <facusbrr@gmail.com>
2024-08-18 03:19:09 -0300

    Se saco un import que no se utilizaba

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import { HOST, PORT, DB } from '../config/db.config.js';
-import { Role, User } from '../models/index.js';
+import { Role } from '../models/index.js';
 
 export const db = {
   mongoose,
